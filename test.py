c38e72ceda0b7298b8a5ac6c4200c6ccce60c425
ArwinJa <arwin.jacob.aj@gmail.com>
2022-07-13 14:50:48 +0200

    Update test.py

--- test.py
+++ test.py
@@ -27,6 +27,6 @@ while go:
         x -= speed
 
     screen.fill((0,0,0))
-    pygame.draw.rect(screen, (0,255,0), (x,y,width,height))
+    pygame.draw.rect(screen, (255,255,0), (x,y,width,height))
     pygame.display.update()
     clock.tick(60)
